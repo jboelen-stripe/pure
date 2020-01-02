9247fa0b33cea857def77f864f9d5703878853ae
jboelen-stripe <39067434+jboelen-stripe@users.noreply.github.com>
2020-01-02 11:30:10 -0800

    Custom Dirty Branch Character

--- pure.zsh
+++ pure.zsh
@@ -482,7 +482,7 @@ prompt_pure_async_callback() {
 			if (( code == 0 )); then
 				unset prompt_pure_git_dirty
 			else
-				typeset -g prompt_pure_git_dirty="*"
+				typeset -g prompt_pure_git_dirty=" ✗"
 			fi
 
 			[[ $prev_dirty != $prompt_pure_git_dirty ]] && do_render=1
